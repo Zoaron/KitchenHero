f0f179e920d8939945eac7bd72af0ba6cf43bb59
Zoaron <kira29125@hotmail.sg>
2017-09-30 17:37:18 +0800

    Kitchen Hero Base

---
+++ KitchenHeroV1.0.0/KitchenHeroV1.0.0/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace KitchenHeroV1._0._0
+{
+    [Activity(Label = "KitchenHeroV1._0._0", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
